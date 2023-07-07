ea2ee557ca00a3e92aae2160006f969a32e571fe
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2023-07-07 21:24:20 +0530

    Global settings and code changes

--- src/entrypoints/index.js
+++ src/entrypoints/index.js
@@ -1,4 +1,9 @@
-import HelloWorld from '../hello-world'
+import ShopTheLook from "./components/shop-the-look";
+import ProductGallery from "./components/product-gallery";
+import VariantSelector from "./components/variant-selector";
+import Product from "./models/product";
 
-window.customElements.define('hello-world', HelloWorld)
-
+window.customElements.define("shop-the-look", ShopTheLook);
+window.customElements.define("product-gallery", ProductGallery);
+window.customElements.define("variant-selector", VariantSelector);
+window.customElements.define("component-product", Product);
--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 import adastra from 'adastra-plugin'
 
 export default defineConfig({
-  plugins: [adastra()]
+  plugins: [adastra()],
+  build: {
+    emptyOutDir: false
+  }
 })
